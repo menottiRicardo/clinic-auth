afbb275930dfd4f8de7b0d67a7a4de1ef11faaa7
ricardomenotti <ricardomenotti31@gmail.com>
2023-12-11 19:02:53 -0500

    health

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Public } from './core/constants';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Public()
+  @Get('health')
   getHello(): string {
     return this.appService.getHello();
   }
